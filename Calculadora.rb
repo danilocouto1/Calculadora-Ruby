bfe5a432338f517f6bc972279de7fae0f60b6f61
Yehior <dansantosalfa@gmail.com>
2023-11-10 17:00:28 -0300

    feat: Criando Loop de menu e opçoes

---
+++ Calculadora.rb
@@ -0,0 +1,11 @@
+opcao = " "
+loop do
+    puts "Bem vindo a calculadora terminal Ruby"
+    puts "Selecione uma opção usando numero :"
+    puts "Adição - 1"
+    puts "Subtração - 2"
+    puts "Multiplicação - 3"
+    puts "Divisão - 4"
+    puts "Sair - 0"
+
+    opcao - gets.chomp.to_i
